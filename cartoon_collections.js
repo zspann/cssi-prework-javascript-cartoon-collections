c0e304db5d320722aa3ba7be6f7037120d8322da
Zack Spann <zspann@zspann-macbookpro4.roam.corp.google.com>
2017-03-17 13:36:54 -0700

    solved 1 & 2, still struggling with 3

--- cartoon_collections.js
+++ cartoon_collections.js
@@ -1,15 +1,52 @@
 function dwarfRollCall(dwarves) {
-	//your code here!
+  dwarves.forEach(function(name, index){
+    var number = index + 1;
+    dwarves[index] = (number + ". " + name + " ");
+  })
+  dwarves = dwarves.join("")
+  return dwarves
 }
 
 function summonCaptainPlanet(planeteerCalls){
-	//your code here!
+	planeteerCalls.forEach(function(call, index){
+    call = call + "!"
+    call = call.toUpperCase();
+    planeteerCalls[index] = call
+  })
+  return planeteerCalls
 }
 
+// I think the test for this is broken - test accepts it but the console it doesn't return the correct results.
 function longPlaneteerCalls(words) {
-	//your code here!
+  var i = 0
+  while (i < words.length){
+    if (words[i].length <= 4){
+      return false;
+      i = i + 1
+    }
+    else {
+      return true
+    }
+  }
 }
 
 function findTheCheese(foods) {
-	// your code here!
+  var i = 0
+  while (i <= foods.length){
+    if (foods[i] != "cheddar" || "gouda" || "camembert"){
+      i = i + 1
+    }
+    else if (foods[i] = "cheddar"){
+      return "cheddar"
+    }
+    else if (foods[i] = "gouda"){
+      return "gouda"
+    }
+    else if (foods[i] = "camembert"){
+      return "camembert"
+    }
+    else {
+      return "no cheese!"
+    }
+  }
 }
